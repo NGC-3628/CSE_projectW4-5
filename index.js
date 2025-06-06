090ef30a0fbe7ed32ee254a1b525667f9134d8da
NGC-3628 <isaacvgn@gmail.com>
2025-06-06 16:39:42 -0600

    athtenticator

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ import { initDb as mongoInitDb } from './data/database.js';
 import 'dotenv/config';
 import routes from './routes/index.js'; 
 import swaggerUi from 'swagger-ui-express';
+import passport, { Passport } from 'passport';
+import session from 'express-session';
+import { Strategy as GitHubStrategy } from 'passport-github2';
+
 
 
 
@@ -21,9 +25,60 @@ const port = process.env.PORT || 8080;
 app
     .use(express.json())
     .use('/', routes)
-    .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+    .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
+//authenticator
+    .use(session({
+        secret: "secret",
+        resave: false,
+        saveUninitialized: true,
+    }))
+    .use(passport.initialize())
+    .use(passport.session())
+    .use((req, res, next) => {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader(
+            "Accesse-Control-Allow-Headers",
+            "Origin, X-Requested-With, Content-type, Accept, Z-Key, Authorization"
+        );
+        res.setHeader(
+            "Accesse-Control-Allow-Headers",
+            "POST, GET, PUT, PACH, OPTION,  DELETE, "
+        );
+        next();
+    })
+    .use(cors({methods: ['POST', 'GET', 'PUT', 'PACH', 'OPTION',  'DELETE']}))
+    .use(cors({ origin: "*"}))
+    .use("/", require("./routes/index.js"));
 
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.CALLBACK_URL
+},
+function(accessToken, refreshToken, profile, done) {
+    return done(null, profile);
+}
+));
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+passport.deserializeUser((user, done) =>{
+    done(null, user);
+});
+
+app.get('/', (req, res) => {
+    res.send(req.session.user !== undefined ? `logged in as ${req.session.user.displayName}` : `Logged out`)
+});
+
+app.get('/github/callback', passport.authenticate('githib', {
+    failureRedirect: 'api-docs', session: false}),
+    (req, res) => {
+        req.session.user = req.user;
+        res.redirect('/');
+    }
+);
 
 
 //first test
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,23 @@
 import express from 'express';
 import studentRoutes from './students.js';
 import teacherRoutes from './teachers.js';
+import passport from 'passport';
 
 const router = express.Router();
 
 router.use('/students', studentRoutes);
 router.use('/teachers', teacherRoutes);
+
+//LOGIN
+router.get('/login', passport.authenticate('github'), (req, res) => {});
+
+
+//LOGOUT
+router.get('/logout', function(req, res, next) {
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+    });
+});
 
 export default router;
